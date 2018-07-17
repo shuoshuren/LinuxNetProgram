0b0a30c6781e4e4e3942de45db4cb6c2ef692de8
shuoshuren <534361124@qq.com>
2018-07-17 17:40:25 +0800

    pipe读写大数据

--- 04/03/pipe.c
+++ 04/03/pipe.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+#define K 1024
+#define WRITELEN (128*K)
+
+
 int main(void){
 
 
@@ -14,7 +18,7 @@ int main(void){
     int *read_fd = &fd[0];
     int *write_fd = &fd[1];
     char string[] = "你好，管道";
-    char readbuffer[80];
+    char readbuffer[10*K];
 
     result = pipe(fd);
 
@@ -28,17 +32,30 @@ int main(void){
         printf("fork 失败\n");
         return -1;
     }else if(0 == pid){//子进程
-        close(*read_fd);
-        
-        result = write(*write_fd,string,strlen(string));
+        int write_size = WRITELEN;
+        result = 0;
 
+        close(*read_fd);
+        while(write_size >=0){
+             result = write(*write_fd,string,strlen(string));
+             if(result >0){
+                write_size -= result;
+                printf("写入%d个数据，剩余%d个数据\n",result,write_size);
+             }else{
+                sleep(10);//等待10s,读端将数据读出
+             }
+        }
         return 0;
     }else {//父进程
         close(*write_fd);
-        nbytes = read(*read_fd,readbuffer,sizeof(readbuffer));
-
-        printf("接收到%d个数据，内容：%s\n",nbytes,readbuffer);
-    
+        while(1){
+            nbytes = read(*read_fd,readbuffer,sizeof(readbuffer));
+            if(nbytes <=0){
+                printf("没有数据写入\n");
+                break;
+            }
+            printf("接收到%d个数据，内容：%s\n",nbytes,readbuffer);
+        }
     }
     return 0;
 
