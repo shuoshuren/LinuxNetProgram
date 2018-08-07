9350cfa708bbc8f5a98824ab3c11207ff9ede6fb
shuoshuren <534361124@qq.com>
2018-08-07 10:44:55 +0800

    工作调度

---
+++ 18/SHTTPD/worker/worker.c
@@ -0,0 +1,156 @@
+//
+// Created by xc on 18-8-3.
+//
+
+#include "worker.h"
+#include "../config/config.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdlib.h>
+#include <memory.h>
+
+/**
+ * 当有客户端连接来时，将客户端连接分配给空闲客户端，由客户端处理请求
+ * @param ss
+ * @return
+ */
+int worker_schedule_run(int ss){
+    printf("==>worker schedule run\n");
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
+    //初始化业务线程
+    worker_init();
+
+    int i = 0;
+    for(;SCHEDULE_STATUS == STATUS_RUNNING;){
+        struct timeval tv;
+        fd_set rfds;
+        int retval = -1;
+        //清空读文件集，将客户端连接描述符放入读文件集
+        FD_ZERO(&rfds);
+        FD_SET(ss,&rfds);
+        tv.tv_sec = 0;
+        tv.tv_usec = 500000;
+
+        retval = select(ss+1,&rfds,NULL,NULL,&tv);
+        switch(retval){
+            case -1:
+            case 0:
+                continue;
+                break;
+            default:
+                if(FD_ISSET(ss,&rfds)){
+                    int sc = accept(ss,(struct sockaddr *)&client,&len);
+                    i = worker_is_status(WORKER_IDEL);//查找空闲业务线程
+                    if(i == -1){
+                        i = worker_is_status(WORKER_DETACHED);//是否到达最大客户端数
+                        if(i != -1){
+                            worker_add(i);
+                        }
+                    }
+                    if(i != -1){
+                        wctls[i].conn.cs = sc;
+                        pthread_mutex_unlock(&wctls[i].opts.mutex);
+
+                    }
+                }
+                break;
+        }
+
+    }
+    printf("<== worker schedule run\n");
+    return 0;
+
+}
+
+/**
+ * 停止调度
+ * @return
+ */
+int worker_schedule_stop(){
+    printf("==>worker schedule stop\n");
+    SCHEDULE_STATUS = STATUS_STOP;
+    int i=0;
+    worker_destroy();
+    int allfired = 0;
+    for(;!allfired;){
+        allfired = 1;
+        for(i =0;i<conf_para.MaxClient;i++){
+            int flags = wctls[i].opts.flags;
+            if(flags == WORKER_DETACHING || flags == WORKER_IDEL){
+                allfired = 0;
+            }
+        }
+    }
+
+    pthread_mutex_destroy(&thread_init);
+    for(i =0;i<conf_para.MaxClient;i++){
+        pthread_mutex_destroy(&wctls[i].opts.mutex);
+    }
+    free(wctls);
+    printf("<== worker schedule stop\n");
+    return 0;
+}
+
+/**
+ * 初始化线程
+ */
+static void worker_init(){
+    printf("==> worker init\n");
+    int i=0;
+    wctls = (struct worker_ctl *)malloc(sizeof(struct worker_ctl) * conf_para.MaxClient);
+    memset(wctls,0, sizeof(*wctls)* conf_para.MaxClient);
+
+    for(i =0;i<conf_para.MaxClient;i++){
+        //opts和conn结构与worker_ctl结构形成回指针
+        wctls[i].opts.work = &wctls[i];
+        wctls[i].conn.work = &wctls[i];
+        //opts 初始化
+        wctls[i].opts.flags = WORKER_DETACHED;
+        pthread_mutex_init(&wctls[i].opts.mutex,NULL);
+        pthread_mutex_lock(&wctls[i].opts.mutex);
+        //conn 初始化
+        wctls[i].conn.con_req.conn = &wctls[i].conn;
+        wctls[i].conn.con_res.conn = &wctls[i].conn;
+        wctls[i].conn.cs = -1;
+        //con_req部分初始化
+        wctls[i].conn.con_req.req.ptr = wctls[i].conn.dreq;
+        wctls[i].conn.con_req.head = wctls[i].conn.dreq;
+        wctls[i].conn.con_req.uri = wctls[i].conn.dreq;
+        //con_res部分初始化
+        wctls[i].conn.con_res.fd = -1;
+        wctls[i].conn.con_res.res.ptr = wctls[i].conn.dres;
+    }
+    for(i = 0;i<conf_para.InitClient;i++){
+        worker_add(i);
+    }
+    printf("<==worker init\n");
+}
+
+static void worker_destroy(){
+
+}
+
+/**
+ * 查找状态相同的worker
+ * @param status
+ * @return -1表示找不到
+ */
+int worker_is_status(int status){
+    for (int i = 0; i < wctls.length; ++i) {
+        if(wctls[i].opts.flags == status){
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ *  添加业务线程
+ * @param position
+ * @return
+ */
+void worker_add(int position){
+    wctls[position].opts.flags == WORKER_RUNNING;
+}
---
+++ 18/SHTTPD/worker/worker.h
@@ -0,0 +1,66 @@
+//
+// Created by xc on 18-8-3.
+//
+
+
+
+#ifndef SHTTPD_WORKER_H
+#define SHTTPD_WORKER_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../base/base.h"
+
+#define STATUS_RUNNING 1
+#define STATUS_STOP 2
+#define WORKER_IDEL -1
+#define WORKER_DETACHED 1
+#define WORKER_DETACHING 2
+#define WORKER_RUNNING 3
+
+static int SCHEDULE_STATUS;
+
+static pthread_mutex_t thread_init;
+
+extern struct worker_ctl wctls[];
+
+
+/**
+ * 当有客户端连接来时，将客户端连接分配给空闲客户端，由客户端处理请求
+ * @param ss
+ * @return
+ */
+int worker_schedule_run(int ss);
+
+/**
+ * 停止调度
+ * @return
+ */
+int worker_schedule_stop();
+
+/**
+ * 初始化线程
+ */
+static void worker_init();
+
+/**
+ * 销毁
+ */
+static void worker_destroy();
+
+/**
+ * 查找状态相同的worker
+ * @param status
+ * @return -1表示找不到
+ */
+int worker_is_status(int status);
+
+/**
+ *  添加业务线程
+ * @param position
+ * @return
+ */
+void worker_add(int position);
+
+#endif //SHTTPD_WORKER_H
