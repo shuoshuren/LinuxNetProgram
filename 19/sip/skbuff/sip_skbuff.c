6beb555d7aa49a60ba24b975c3acadca9f89a896
shuoshuren <534361124@qq.com>
2018-08-08 17:47:19 +0800

    skbuffer

--- 19/sip/skbuff/sip_skbuff.c
+++ 19/sip/skbuff/sip_skbuff.c
@@ -3,3 +3,36 @@
 //
 
 #include "sip_skbuff.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+
+/**
+ * 申请用户指定大小缓存区域
+ * @param size
+ * @return
+ */
+struct skbuff * skb_alloc(__uint32_t size){
+    printf("==>skb alloc\n");
+    struct skbuff *skb = (struct skbuff *)malloc(sizeof(struct skbuff));
+    if(!skb){
+        printf("==> malloc skb header error\n");
+        return NULL;
+    }
+    memset(skb,0,sizeof(struct skbuff));//初始化skbuff内存结构
+    skb->head = (__uint8_t *) malloc(size);//申请数据区域内存，并保存在head指针中
+    if(!skb->head){
+        printf("==> malloc skb data error\n");
+        free(skb);//释放之前生气的skbuff内存
+        return NULL;
+    }
+    memset(skb->head,0,size);//初始化用户内存区
+    skb->end = skb->head+size;//end指针初始化
+    skb->data = skb->head;//data指针初始化和head一致
+    skb->tail = skb->data;//tail指针最初和data一致
+    skb->next = NULL;
+    skb->tot_len = 0;//有用数据总长度为0
+    skb->len = 0;//当前结构中数据长度为0
+    printf("<==skb alloc\n");
+    return skb;
+}
--- 19/sip/skbuff/sip_skbuff.h
+++ 19/sip/skbuff/sip_skbuff.h
@@ -6,4 +6,52 @@
 #ifndef SIP_SIP_SKBUFF_H
 #define SIP_SIP_SKBUFF_H
 
+#include <zconf.h>
+
+/**
+ * 存储缓冲结构，主要包含各个不同层次的协议结构指针，控制指针和网络数据指针
+ */
+struct skbuff{
+    struct skbuff *next;//下一个skbuff结构
+    union{
+        struct sip_tcphdr *tcph;//TCP协议头指针
+        struct sip_udphdr *udph;//UDP
+        struct sip_icmphdr *icmph;//ICMP
+        struct sip_igmphdr *igmph;//IGMP
+        __uint8_t  *raw;//传输层原始数据
+    } th;//传输层变量
+    union{
+        struct sip_iphdr *iph;//ip
+        struct sip_arphdr *arph;//arp
+        __uint8_t *raw;//网络层原始数据
+    } nh;//网络层
+    union{
+        struct sip_ethhdr *ethh;//物理层以太网头部
+        __uint8_t *raw;//物理层变量
+    } phy;//物理层
+    struct net_device *dev;//网卡设备
+    __int16_t protocol;//协议类型
+    __uint32_t tot_len;//skbuff中网络数据的总长度
+    __uint32_t len;//skbuff当前协议层数据长度
+    __uint8_t csum;//校验和
+    __uint8_t ip_summed;//ip层头部是否进行了校验
+    __uint8_t *head;//实际网络层数据的头部指针
+    __uint8_t *data;//当前层网络数据的头部指针
+    __uint8_t *tail;//当前层网络数据的尾部指针
+    __uint8_t *end;//实际网络数据的尾部指针
+};
+
+/**
+ * 申请用户指定大小缓存区域
+ * @param size
+ * @return
+ */
+struct skbuff * skb_alloc(__uint32_t size);
+
+void skb_free();
+
+void skb_clone();
+
+void skb_put();
+
 #endif //SIP_SIP_SKBUFF_H
