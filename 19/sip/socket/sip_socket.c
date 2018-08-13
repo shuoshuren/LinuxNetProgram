5f3c21bfbc6cfdd194b98e574a0906a6f9fdb4b9
shuoshuren <534361124@qq.com>
2018-08-13 14:35:43 +0800

    skbuff sock层相关代码

--- 19/sip/socket/sip_socket.c
+++ 19/sip/socket/sip_socket.c
@@ -2,4 +2,55 @@
 // Created by xc on 18-8-8.
 //
 
+#include <semaphore.h>
 #include "sip_socket.h"
+#include "../udp/sip_udp.h"
+
+
+/**
+ * bind
+ * @param sock
+ * @param addr
+ * @param port
+ * @return
+ */
+int sip_sock_bind(struct sock *sock,struct in_addr *addr,__uint16_t port){
+    if(sock->pcb.tcp != NULL){
+        switch (sock->type){
+            case SOCK_RAW:
+                break;
+            case SOCK_DGRAM:
+                sock->err = sip_udp_bind(sock->pcb.udp,addr,port);
+                break;
+            case SOCK_STREAM:
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+/**
+ * 接收函数
+ * @param sock
+ * @return
+ */
+struct skbuff * sip_sock_recv(struct sock *sock){
+    struct skbuff *skb_recv = NULL;
+    int num = 0;
+    if(sem_getvalue(&sock->sem_recv,&num)){//没有获取到网络数据，超时等待
+        struct timespec timeout;
+        timeout.tv_sec = sock->recv_timeout;
+        timeout.tv_nsec = 0;
+        sem_timedwait(&sock->sem_recv,&timeout);
+    }else{//已有数据，直接获取数据
+        sem_wait(&sock->sem_recv);
+    }
+    skb_recv = sock->skb_recv;
+    if(skb_recv == NULL){
+        return NULL;
+    }
+    sock->skb_recv = skb_recv->next;
+    skb_recv->next = NULL;
+    return skb_recv;
+}
--- 19/sip/socket/sip_socket.h
+++ 19/sip/socket/sip_socket.h
@@ -7,12 +7,41 @@
 #define SIP_SIP_SOCKET_H
 
 #include <bits/semaphore.h>
+#include <arpa/inet.h>
 #include "../skbuff/sip_skbuff.h"
 
 
-struct sock{
-    struct skbuff *skb_recv;
-     sem_t *sem_recv;
+
+struct sock {
+    int type;//协议类型
+    int state;//协议状态
+    union {
+        struct ip_pcb *ip;//ip层控制结构
+        struct tcp_pcb *tcp;//tcp层控制结构
+        struct udp_pcb *udp;//udp层控制结构
+    } pcb;
+    int err;//错误值
+    struct skbuff *skb_recv;//接收缓冲区
+    sem_t *sem_recv;//接受缓冲区计数信号量
+    int socket;//socket对应的文件描述符
+    int recv_timeout;//接收数据超时时间
+    __uint16_t recv_avail;//可以接收数据
 };
+
+/**
+ * bind
+ * @param sock
+ * @param addr
+ * @param port
+ * @return
+ */
+int sip_sock_bind(struct sock *sock,struct in_addr *addr,__uint16_t port);
+
+/**
+ * 接收函数
+ * @param sock
+ * @return
+ */
+struct skbuff * sip_sock_recv(struct sock *sock);
 
 #endif //SIP_SIP_SOCKET_H
